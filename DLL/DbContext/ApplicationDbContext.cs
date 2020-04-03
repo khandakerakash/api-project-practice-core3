e78920b305a114c69b2982edd54e338cf48f69e5
khandakerakash <akash.cse10@gmail.com>
2020-04-04 00:29:01 +0600

    SoftDelete is implemented.

--- DLL/DbContext/ApplicationDbContext.cs
+++ DLL/DbContext/ApplicationDbContext.cs
@@ -1,4 +1,10 @@
-﻿using DLL.Model;
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using DLL.Model;
+using DLL.Model.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace DLL.DbContext
@@ -11,5 +17,72 @@ namespace DLL.DbContext
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                if (typeof(ISoftDeletable).IsAssignableFrom(entity.ClrType) == true)
+                {
+                    entity.AddProperty(IsDeletedProperty, typeof(bool));
+
+                    modelBuilder
+                        .Entity(entity.ClrType)
+                        .HasQueryFilter(GetIsDeletedRestriction(entity.ClrType));
+                }
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+        
+        private const string IsDeletedProperty = "IsDeleted";
+        private static readonly MethodInfo _propertyMethod = typeof(EF).GetMethod(nameof(EF.Property), BindingFlags.Static | BindingFlags.Public)?.MakeGenericMethod(typeof(bool));
+
+        private static LambdaExpression GetIsDeletedRestriction(Type type)
+        {
+            var parm = Expression.Parameter(type, "it");
+            var prop = Expression.Call(_propertyMethod, parm, Expression.Constant(IsDeletedProperty));
+            var condition = Expression.MakeBinary(ExpressionType.Equal, prop, Expression.Constant(false));
+            var lambda = Expression.Lambda(condition, parm);
+            return lambda;
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken())
+        {
+            OnBeforeSaving();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void OnBeforeSaving()
+        {
+            var entries = ChangeTracker.Entries();
+            foreach (var entry in entries)
+            {
+                if (entry.Entity is ITrackable trackable)
+                {
+                    var now = DateTime.UtcNow;
+
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            trackable.CreatedAt = now;
+                            trackable.UpdatedAt = now;
+                            break;
+                        case EntityState.Modified:
+                            trackable.UpdatedAt = now;
+                            break;
+                        case EntityState.Deleted:
+                            entry.Property(IsDeletedProperty).CurrentValue = true;
+                            entry.State = EntityState.Modified;
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
---
+++ DLL/Model/Interfaces/ITrackable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DLL.Model.Interfaces
+{
+    public interface ITrackable
+    {
+        DateTimeOffset CreatedAt { get; set; }
+        string CreatedBy { get; set; }
+        DateTimeOffset UpdatedAt { get; set; }
+        string UpdatedBy { get; set; }
+    }
+}
